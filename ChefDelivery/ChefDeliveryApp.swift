e066382ab1547ed32e25f5d9762baafe96af09de
Douglas Silva <douglasferreirasilva.si@gmail.com>
2026-02-16 10:54:46 -0300

    Initial Commit

---
+++ ChefDelivery/ChefDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliveryApp.swift
+//  ChefDelivery
+//
+//  Created by Douglas Silva on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
